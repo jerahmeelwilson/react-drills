4565f372aa2c6093c985ebf727073e5c27885cae
jerahmeelwilson <wilsonjerahmeel@gmail.com>
2021-12-20 02:19:03 -0600

    Completed app 1 class component

--- app-1/src/components/Reflection.js
+++ app-1/src/components/Reflection.js
@@ -1,16 +1,41 @@
-import React, {useState}from 'react'
+import React, { Component } from "react";
 
-export default function Reflection() {
-    const [userInput, setUserInput] = useState("")
-
-    function displayInput(event){
-        setUserInput(event.target.value)
+export default class Reflection extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        inputValue:""
     }
-    
+    this.displayInput = this.displayInput.bind(this);
+  }
+
+  displayInput(event){
+      this.setState({inputValue:event.target.value})
+  }
+
+  render() {
     return (
-        <div>
-            <input onChange={displayInput}value={userInput}></input>
-            <p>{userInput}</p>
-        </div>
-    )
+      <div>
+        <input onChange={this.displayInput}></input>
+        <p>{this.state.inputValue}</p>
+      </div>
+    );
+  }
 }
+
+// import React, {useState}from 'react'
+
+// export default function Reflection() {
+//     const [userInput, setUserInput] = useState("")
+
+//     function displayInput(event){
+//         setUserInput(event.target.value)
+//     }
+
+//     return (
+//         <div>
+//             <input onChange={displayInput}></input>
+//             <p>{userInput}</p>
+//         </div>
+//     )
+// }
